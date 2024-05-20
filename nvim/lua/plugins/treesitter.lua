ba22d8e80736e36c56cbb916396103fffaccbd75
Magnus Root <magnus-root@tzeentch.ru>
2024-05-20 15:19:30 +0300

    Добавлен конфиг

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "typescript", "lua", "go", "tsx", "javascript", "c", "python", "html", "css" },
+
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+}
